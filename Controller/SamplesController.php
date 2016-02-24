46250145e1f9004a5dffd66618d594a0aaa09748
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-02-24 12:02:07 +0900

    メール送信コントローラー debug用

---
+++ Controller/SamplesController.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * メール送信サンプル Controller
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('AppController', 'Controller');
+App::uses('CakeEmail', 'Network/Email');
+App::uses('NetCommonsMail', 'Mails.Utility');
+
+/**
+ * メール送信サンプル Controller
+ * debug用
+ * 削除する予定
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @package NetCommons\Mails\Controller
+ */
+class SamplesController extends AppController {
+
+/**
+ * メール送信サンプル
+ *
+ * @return CakeResponse
+ */
+	public function mailtest() {
+		$email = new CakeEmail('sakura');						// インスタンス化
+		$email->to('mutaguchi@opensource-workshop.jp');			// 送信先
+		$email->subject('メールタイトル');						// メールタイトル
+
+		$email->send('メール本文');								// メール送信
+	}
+
+/**
+ * メール送信サンプル２
+ *
+ * @return CakeResponse
+ */
+	public function mailtest2() {
+		$mail = new NetCommonsMail();
+		$mail->send2();
+
+		//		MailSend::MailSend();
+	}
+}
