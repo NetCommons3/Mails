f0ead0fbe16670a4340d09ff873c3f2e524416fb
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-03-07 06:23:08 +0900

    beforeValidate()に移動

--- Model/MailQueue.php
+++ Model/MailQueue.php
@@ -20,91 +20,96 @@ App::uses('MailsAppModel', 'Mails.Model');
 class MailQueue extends MailsAppModel {
 
 /**
- * Use database config
- *
- * @var string
- */
-	public $useDbConfig = 'master';
-
-/**
  * Validation rules
  *
  * @var array
  */
-	public $validate = array(
-		'plugin_key' => array(
-			'notBlank' => array(
-				'rule' => array('notBlank'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				'required' => true,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		//		'block_key' => array(
-		//			'notBlank' => array(
-		//				'rule' => array('notBlank'),
-		//				//'message' => 'Your custom message here',
-		//				//'allowEmpty' => false,
-		//				//'required' => false,
-		//				//'last' => false, // Stop validation after this rule
-		//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-		//			),
-		//		),
-		'replay_to' => array(
-			'email' => array(
-				'rule' => array('email'),
-				//'message' => 'Your custom message here',
-				'allowEmpty' => true,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		//		'content_key' => array(
-		//			'notBlank' => array(
-		//				'rule' => array('notBlank'),
-		//				//'message' => 'Your custom message here',
-		//				//'allowEmpty' => false,
-		//				//'required' => false,
-		//				//'last' => false, // Stop validation after this rule
-		//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-		//			),
-		//		),
-		'mail_subject' => array(
-			'notBlank' => array(
-				'rule' => array('notBlank'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				'required' => true,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'mail_body' => array(
-			'notBlank' => array(
-				'rule' => array('notBlank'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				'required' => true,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'send_time' => array(
-			'datetime' => array(
-				'rule' => array('datetime'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				'required' => true,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
+	public $validate = array();
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+/**
+ * beforeValidate
+ *
+ * @param array $options Options passed from Model::save().
+ * @return bool True if validate operation should continue, false to abort
+ * @link http://book.cakephp.org/2.0/ja/models/callback-methods.html#beforevalidate
+ * @see Model::save()
+ */
+	public function beforeValidate($options = array()) {
+		$this->validate = Hash::merge($this->validate, array(
+			'plugin_key' => array(
+				'notBlank' => array(
+					'rule' => array('notBlank'),
+					//'message' => 'Your custom message here',
+					//'allowEmpty' => false,
+					'required' => true,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+			//		'block_key' => array(
+			//			'notBlank' => array(
+			//				'rule' => array('notBlank'),
+			//				//'message' => 'Your custom message here',
+			//				//'allowEmpty' => false,
+			//				//'required' => false,
+			//				//'last' => false, // Stop validation after this rule
+			//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			//			),
+			//		),
+			'replay_to' => array(
+				'email' => array(
+					'rule' => array('email'),
+					//'message' => 'Your custom message here',
+					'allowEmpty' => true,
+					//'required' => false,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+			//		'content_key' => array(
+			//			'notBlank' => array(
+			//				'rule' => array('notBlank'),
+			//				//'message' => 'Your custom message here',
+			//				//'allowEmpty' => false,
+			//				//'required' => false,
+			//				//'last' => false, // Stop validation after this rule
+			//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			//			),
+			//		),
+			'mail_subject' => array(
+				'notBlank' => array(
+					'rule' => array('notBlank'),
+					//'message' => 'Your custom message here',
+					//'allowEmpty' => false,
+					'required' => true,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+			'mail_body' => array(
+				'notBlank' => array(
+					'rule' => array('notBlank'),
+					//'message' => 'Your custom message here',
+					//'allowEmpty' => false,
+					'required' => true,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+			'send_time' => array(
+				'datetime' => array(
+					'rule' => array('datetime'),
+					//'message' => 'Your custom message here',
+					//'allowEmpty' => false,
+					'required' => true,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+		));
+
+		return parent::beforeValidate($options);
+	}
 
 /**
  * hasMany associations
--- Model/MailQueueUser.php
+++ Model/MailQueueUser.php
@@ -20,81 +20,86 @@ App::uses('MailsAppModel', 'Mails.Model');
 class MailQueueUser extends MailsAppModel {
 
 /**
- * Use database config
- *
- * @var string
- */
-	public $useDbConfig = 'master';
-
-/**
  * Validation rules
  *
  * @var array
  */
-	public $validate = array(
-		'plugin_key' => array(
-			'notBlank' => array(
-				'rule' => array('notBlank'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				'required' => true,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		//		'block_key' => array(
-		//			'notBlank' => array(
-		//				'rule' => array('notBlank'),
-		//				//'message' => 'Your custom message here',
-		//				//'allowEmpty' => false,
-		//				//'required' => false,
-		//				//'last' => false, // Stop validation after this rule
-		//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-		//			),
-		//		),
-		'mail_queue_id' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				'required' => true,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'user_id' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				'allowEmpty' => true,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'room_id' => array(
-			'numeric' => array(
-				'rule' => array('numeric'),
-				//'message' => 'Your custom message here',
-				'allowEmpty' => true,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'to_address' => array(
-			'email' => array(
-				'rule' => array('email'),
-				//'message' => 'Your custom message here',
-				'allowEmpty' => true,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-	);
+	public $validate = array();
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+/**
+ * beforeValidate
+ *
+ * @param array $options Options passed from Model::save().
+ * @return bool True if validate operation should continue, false to abort
+ * @link http://book.cakephp.org/2.0/ja/models/callback-methods.html#beforevalidate
+ * @see Model::save()
+ */
+	public function beforeValidate($options = array()) {
+		$this->validate = Hash::merge($this->validate, array(
+			'plugin_key' => array(
+				'notBlank' => array(
+					'rule' => array('notBlank'),
+					//'message' => 'Your custom message here',
+					//'allowEmpty' => false,
+					'required' => true,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+			//		'block_key' => array(
+			//			'notBlank' => array(
+			//				'rule' => array('notBlank'),
+			//				//'message' => 'Your custom message here',
+			//				//'allowEmpty' => false,
+			//				//'required' => false,
+			//				//'last' => false, // Stop validation after this rule
+			//				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			//			),
+			//		),
+			'mail_queue_id' => array(
+				'numeric' => array(
+					'rule' => array('numeric'),
+					//'message' => 'Your custom message here',
+					//'allowEmpty' => false,
+					'required' => true,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+			'user_id' => array(
+				'numeric' => array(
+					'rule' => array('numeric'),
+					//'message' => 'Your custom message here',
+					'allowEmpty' => true,
+					//'required' => false,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+			'room_id' => array(
+				'numeric' => array(
+					'rule' => array('numeric'),
+					//'message' => 'Your custom message here',
+					'allowEmpty' => true,
+					//'required' => false,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+			'to_address' => array(
+				'email' => array(
+					'rule' => array('email'),
+					//'message' => 'Your custom message here',
+					'allowEmpty' => true,
+					//'required' => false,
+					//'last' => false, // Stop validation after this rule
+					//'on' => 'create', // Limit validation to 'create' or 'update' operations
+				),
+			),
+		));
+
+		return parent::beforeValidate($options);
+	}
 
 /**
  * belongsTo associations
