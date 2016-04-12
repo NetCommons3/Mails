8a168d23e2b001c1e2d3a2d1383ec851c83c7194
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-04-12 14:14:05 +0900

    マイグレーション：not_send_room_user_ids項目追加

---
+++ Config/Migration/1460437758_add_columns_not_send_room_user_ids.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Migration file
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+/**
+ * Mails CakeMigration
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @package NetCommons\Mails\Config\Migration
+ */
+class AddColumnsNotSendRoomUserIds extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = 'add_columns_not_send_room_user_ids';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_field' => array(
+				'mail_queue_users' => array(
+					'not_send_room_user_ids' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'comment' => 'ルーム送信時に送らない複数のユーザ', 'charset' => 'utf8mb4', 'after' => 'to_address'),
+				),
+			),
+		),
+		'down' => array(
+			'drop_field' => array(
+				'mail_queue_users' => array('not_send_room_user_ids'),
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction Direction of migration process (up or down)
+ * @return bool Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
