a4279c626e360847d67c704355bf516c6be57d48
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-05-04 13:52:48 +0900

    phpdoc追記

--- Config/Migration/1462334363_add_columns_send_room_permission.php
+++ Config/Migration/1462334363_add_columns_send_room_permission.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Migration file
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+/**
+ * Mails CakeMigration
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @package NetCommons\Mails\Config\Migration
+ */
 class AddColumnsSendRoomPermission extends CakeMigration {
 
 /**
