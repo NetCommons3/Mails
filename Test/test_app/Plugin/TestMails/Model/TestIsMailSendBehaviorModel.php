80b2a8c737f8cc45dc66db1961985da8c0d6b28f
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-05-17 15:44:52 +0900

    test_app追加

---
+++ Test/test_app/Plugin/TestMails/Model/TestIsMailSendBehaviorModel.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * IsMailSendBehaviorテスト用Model
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('AppModel', 'Model');
+
+/**
+ * IsMailSendBehaviorテスト用Model
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @package NetCommons\Mails\Test\test_app\Plugin\TestMails\Model
+ */
+class TestIsMailSendBehaviorModel extends AppModel {
+
+/**
+ * テーブル名
+ *
+ * @var mixed
+ */
+	public $useTable = false;
+
+/**
+ * 使用ビヘイビア
+ *
+ * @var array
+ */
+	public $actsAs = array(
+		'Mails.IsMailSend'
+	);
+
+}
