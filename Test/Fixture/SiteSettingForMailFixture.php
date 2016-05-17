695688b9c7eb908d4559de374379e7136ce09b30
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-05-17 14:55:45 +0900

    基のFixtureを継承して作成する方法に修正

--- Test/Fixture/SiteSettingForMailFixture.php
+++ Test/Fixture/SiteSettingForMailFixture.php
@@ -9,10 +9,12 @@
  * @copyright Copyright 2014, NetCommons Project
  */
 
+App::uses('SiteSettingFixture', 'SiteManager.Test/Fixture');
+
 /**
  * Summary for SiteSettingFixture
  */
-class SiteSettingForMailFixture extends CakeTestFixture {
+class SiteSettingForMailFixture extends SiteSettingFixture {
 
 /**
  * Full Table Name
@@ -22,59 +24,13 @@ class SiteSettingForMailFixture extends CakeTestFixture {
 	public $table = 'site_settings';
 
 /**
- * Fields
- *
- * @var array
- */
-	public $fields = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'language_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 6),
-		'key' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'Key of the record.\\ne.g.) theme_name, site_name', 'charset' => 'utf8'),
-		'value' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'Value of the record.\\ne.g.) default, My Homepage', 'charset' => 'utf8'),
-		'label' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'Human friendly label for the record.\\ne.g.) Theme, Site Name', 'charset' => 'utf8'),
-		'weight' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'Display order.'),
-		'created_user' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1)
-		),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
-	);
-
-/**
  * Records
  *
  * @var array
  */
 	public $records = array(
-		//		array(
-		//			'id' => '2',
-		//			'language_id' => '2',
-		//			'key' => 'theme',
-		//			'value' => 'UnitTestTheme',
-		//			'label' => 'Theme',
-		//			'weight' => '1',
-		//		),
-		//		array(
-		//			'id' => '3',
-		//			'language_id' => '2',
-		//			'key' => 'App.default_timezone',
-		//			'value' => 'Asia/Tokyo',
-		//			'label' => 'SiteTimezone',
-		//			'weight' => '1',
-		//		),
-		//		array(
-		//			'id' => '4',
-		//			'language_id' => '1',
-		//			'key' => 'App.default_timezone',
-		//			'value' => 'Asia/Tokyo',
-		//			'label' => 'SiteTimezone',
-		//			'weight' => '1',
-		//		),
 		array(
-			'id' => '5',
+			'id' => '1',
 			'language_id' => '0',
 			'key' => 'Mail.from',
 			'value' => 'from@dummy.com',
