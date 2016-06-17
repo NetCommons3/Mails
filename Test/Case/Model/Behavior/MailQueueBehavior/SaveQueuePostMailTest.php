e415a02a6b5bcc61b49d87bcd5ef4eac77bae0d5
Mitsuru Mutaguchi <akagane99@gmail.com>
2016-06-17 18:25:31 +0900

    テスト追加 - Behavior/MailQueueBehavior/SaveQueuePostMailTest

---
+++ Test/Case/Model/Behavior/MailQueueBehavior/SaveQueuePostMailTest.php
@@ -0,0 +1,155 @@
+<?php
+/**
+ * MailQueueBehavior::saveQueuePostMail()のテスト
+ *
+ * @author Noriko Arai <arai@nii.ac.jp>
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @link http://www.netcommons.org NetCommons Project
+ * @license http://www.netcommons.org/license.txt NetCommons License
+ * @copyright Copyright 2014, NetCommons Project
+ */
+
+App::uses('NetCommonsModelTestCase', 'NetCommons.TestSuite');
+App::uses('MailSettingFixedPhrase', 'Mails.Model');
+App::uses('MailQueueBehavior', 'Mails.Model/Behavior');
+App::uses('TestMailQueueBehaviorSaveModelFixture', 'Mails.Test/Fixture');
+
+
+/**
+ * MailQueueBehavior::saveQueuePostMail()のテスト
+ *
+ * @author Mitsuru Mutaguchi <mutaguchi@opensource-workshop.jp>
+ * @package NetCommons\Mails\Test\Case\Model\Behavior\MailQueueBehavior
+ */
+class MailQueueBehaviorSaveQueuePostMailTest extends NetCommonsModelTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'plugin.mails.mail_setting',
+		'plugin.mails.mail_setting_fixed_phrase',
+		//'plugin.mails.site_setting_for_mail',
+		'plugin.site_manager.site_setting',
+		'plugin.mails.test_mail_queue_behavior_save_model',
+		'plugin.rooms.default_role_permission4test',
+		'plugin.rooms.room_role',
+		//'plugin.rooms.room_role_permission4test',
+		'plugin.mails.room_role_permission_for_mail',
+		'plugin.user_attributes.user_role_setting4test',
+	);
+
+/**
+ * Plugin name
+ *
+ * @var string
+ */
+	public $plugin = 'mails';
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+
+		//テストプラグインのロード
+		NetCommonsCakeTestCase::loadTestPlugin($this, 'Mails', 'TestMails');
+		$this->TestModel = ClassRegistry::init('TestMails.TestMailQueueBehaviorSaveModel');
+
+		$this->MailQueueUser = ClassRegistry::init('Mails.MailQueueUser', true);
+	}
+
+/**
+ * saveQueuePostMail()テストのDataProvider
+ *
+ * ### 戻り値
+ *  - settingKey セッティングのキー
+ *  - settingValue セッティングの値
+ *
+ * @return array データ
+ */
+	public function dataProvider() {
+		$data = array(
+			'TestMailQueueBehaviorSaveModel' => (new TestMailQueueBehaviorSaveModelFixture())
+				->records[1],
+		);
+		$result[] = array(
+			'data' => $data,
+			'languageId' => 2,
+			'sendTimes' => null,
+			'userIds' => array(10),
+			'toAddresses' => null,
+			'roomId' => null,
+			'typeKey' => MailSettingFixedPhrase::DEFAULT_TYPE,
+		);
+
+		return $result;
+	}
+
+/**
+ * saveQueuePostMail()のテスト
+ *
+ * @param array $data 登録データ
+ * @param int $languageId 言語ID
+ * @param array $sendTimes メール送信日時 配列
+ * @param array $userIds 送信ユーザID 配列
+ * @param array $toAddresses 送信先メールアドレス 配列
+ * @param int $roomId ルームID
+ * @param string $typeKey メールの種類
+ * @dataProvider dataProvider
+ * @return void
+ */
+	public function testSaveQueuePostMail($data, $languageId, $sendTimes = null, $userIds = null,
+										$toAddresses = null, $roomId = null,
+										$typeKey = MailSettingFixedPhrase::DEFAULT_TYPE) {
+		$this->TestModel->data = $data;
+		$this->TestModel->setSetting('pluginKey', 'dummy_check');
+
+		//テスト実施
+		/** @see MailQueueBehavior::saveQueuePostMail() */
+		$this->TestModel->saveQueuePostMail($languageId, $sendTimes, $userIds,
+			$toAddresses, $roomId, $typeKey);
+
+		//チェック
+		$results = $this->MailQueueUser->find('all', array(
+			'recursive' => -1,
+			'conditions' => array(
+				'plugin_key' => 'dummy_check',
+				'user_id' => $userIds,
+			)
+		));
+		//debug($results);
+		$this->assertCount(1, $results);
+	}
+
+/**
+ * saveQueuePostMail()の例外テスト - Plugin.key=nullでvalidateエラーのため発生
+ *
+ * @param array $data 登録データ
+ * @param int $languageId 言語ID
+ * @param array $sendTimes メール送信日時 配列
+ * @param array $userIds 送信ユーザID 配列
+ * @param array $toAddresses 送信先メールアドレス 配列
+ * @param int $roomId ルームID
+ * @param string $typeKey メールの種類
+ * @dataProvider dataProvider
+ * @return void
+ */
+	public function testSaveQueuePostMailException($data, $languageId, $sendTimes = null, $userIds = null,
+										$toAddresses = null, $roomId = null,
+										$typeKey = MailSettingFixedPhrase::DEFAULT_TYPE) {
+		$this->TestModel->data = $data;
+		$this->setExpectedException('InternalErrorException');
+
+		$this->TestModel->setSetting('pluginKey', null);
+
+		//テスト実施
+		/** @see MailQueueBehavior::saveQueuePostMail() */
+		$this->TestModel->saveQueuePostMail($languageId, $sendTimes, $userIds,
+			$toAddresses, $roomId, $typeKey);
+	}
+}
